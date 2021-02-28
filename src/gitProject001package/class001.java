b7e14ec13d2e3dd3fd4500400001db7b34625c92
Fatma Turkan <fatma.turkan.ince@gmail.com>
2021-02-28 09:57:53 -0600

    third commit

--- src/gitProject001package/class001.java
+++ src/gitProject001package/class001.java
@@ -5,6 +5,8 @@ public class class001 {
 	public static void main(String[] args) {
 		System.out.println("Git second day");
 		System.out.println("New commit");
+		
+		//Welcome to git sessions
 	}
 
 }
